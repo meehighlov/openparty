7403785076ead1ca53f11ec540c3616605361680
Ivan Mikhailov <meehighlov@Ivans-MacBook-Air.local>
2023-01-01 19:52:23 +0300

    http client

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use openparty::config::Config;
 use openparty::telegram;
+use openparty::params::Params;
+
 
 fn main() {
-    let cfg = Config::build();
+    let cfg: Config = Config::build();
+    let params: Params = Params{ cfg: Option(cfg) };
+    
     
 }
--- src/telegram/client.rs
+++ src/telegram/client.rs
@@ -1,34 +1,61 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, iter::Map};
+use ureq;
 use crate::telegram::models::Update;
 
 pub struct Client {
-    token: String,
-    base_url: String,
+    base_url: &'static str,
     updates_url: &'static str,
 }
 
-pub struct Request {}
-
-pub struct Response {}
+struct Request{
+    uri: String,
+    query: String,
+    body: String,
+}
+struct Response<T>{body: T}
 
 
 impl Client {
 
-    pub fn build(token: String, base_url: String) -> Client {
+    pub fn build(token: String) -> Client {
+        let mut base_url: String = "https://api.telegram.org/bot".to_string();
+        base_url.push_str(&token);
+
         Client {
-            token,
-            base_url,
+            base_url: base_url.as_str(),
             updates_url: "/getUpdates",
         }
     }
 
-    pub fn call(url_tail: String, params: HashMap<String, String>) -> Response {
+    fn make_uri<'a>(base: &'a str, tail: &'a str) -> &'a str {
+        return format!("{base}{tail}").as_str();
+    }
 
-        Response {  }
+    fn add_query(url: &str, query: HashMap<String, String>) -> String {
+        let mut url_with_query: String = format!("{url}");
+        for (key, value) in query {
+            let param: String = format!("{key}={value}");
+            url_with_query.push_str(&param);
+        }
+
+        url_with_query
+    }
+
+    fn get(&self, url: &str, query: &str) {
+        let response = match ureq::get(url).call() {
+            
+        } 
+    }
+
+    fn call(&self, method: &str, url_tail: &str, query: &str, params: HashMap<String, String>) {
+        let response = match method.to_lowercase() {
+            case "get" => self.get(query);
+        }
+        
     }
 
     pub fn getUpdates(&self) -> Vec<Update> {
-        let updates = Vec::new();
+        let updates: Vec<Update> = Vec::new();
 
         
 
